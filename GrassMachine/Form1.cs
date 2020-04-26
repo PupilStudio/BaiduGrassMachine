d8669800c1833ef896df814a82c8f807e8c5871f
Peter Ho <PupilStudio@outlook.com>
2020-04-26 21:41:22 +0800

    添加项目文件。

---
+++ GrassMachine/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GrassMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cnt;
+            if (!int.TryParse(textBox2.Text, out cnt) || cnt < 2 || cnt > 500)
+            {
+                MessageBox.Show("翻译次数无效。翻译次数应当是一个整数，并且介于2至500之间。");
+                return;
+            }
+            //textBox3.Text = TranslateHelper.Grass(textBox1.Text, cnt);
+            textBox3.Text = "生草中, 请稍等...";
+            textBox3.Text = TranslateHelper.Grass(textBox1.Text, cnt, textBox3);
+        }
+    }
+}
---
+++ GrassMachine/TranslateHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrassMachine
+{
+    public static class TranslateHelper
+    {
+        private static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+
+        static void Delay(long millSec)
+        {
+            long time = System.DateTime.UtcNow.Ticks;
+            while ((System.DateTime.UtcNow.Ticks - time) / 10000 < millSec)
+                Application.DoEvents();
+
+        }
+
+        public const string APP_ID = "20200426000430735";
+        public const string APP_KEY = "TF3ufAP2qLR2lBcUR17v";
+        public static string Grass(string doc, int times, TextBox view)
+        {
+            if (times <= 1 || times > 500)
+                throw new Exception();
+
+            string cur = doc;
+            string from = "zh";
+
+            string[] lang = new string[] {
+                "zh", "en", "yue", "wyw", "jp", "kor", "fra", "spa", "th", "ara", "ru", "pt",
+                "de", "it", "el", "nl", "pl", "bul", "est", "dan", "fin", "cs", "rom", "slo",
+                "swe", "hu", "vie"
+            };
+
+            
+            var random = new Random();
+            
+            for (int i = 1; i <= times; ++i)
+            {
+                BEGIN:
+                string salt = random.Next(100000).ToString();
+                string to = lang[random.Next(0, lang.Length)];
+                string retJson;
+                if (i == times)
+                    to = "zh";
+                Application.DoEvents();
+                string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?"
+                    + "q=" + Uri.EscapeDataString(cur)
+                    + "&from=" + from
+                    + "&to=" + to
+                    + "&appid=" + APP_ID
+                    + "&salt=" + salt
+                    + "&sign=" + EncryptString(APP_ID + cur + salt + APP_KEY);
+                Application.DoEvents();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "text/html;charset=UTF-8";
+                request.UserAgent = null;
+                request.Timeout = 6000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+                retJson = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+                Application.DoEvents();
+                JObject jParser = JObject.Parse(retJson);
+
+                if ()
+                cur = Uri.UnescapeDataString(jParser["trans_result"][0]["dst"].ToString());
+
+                Delay(1500);
+                System.Diagnostics.Debug.WriteLine(cur);
+                from = to;
+
+                view.Text = "生草中...第" + i.ToString() + "次\r\n" + cur;
+                Application.DoEvents();
+            }
+            return cur;
+        }
+    }
+}
